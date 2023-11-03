4e23cb7fdaa8a8eb15727e2deef4c2ca029aab46
生田一樹 <i.k@ikutakazukinoMacBook-Pro.local>
2023-11-03 18:09:00 +0900

    go学習

--- hello/hello.go
+++ hello/hello.go
@@ -8,21 +8,17 @@ import (
 )
 
 func main() {
-    // Set properties of the predefined Logger, including
-    // the log entry prefix and a flag to disable printing
-    // the time, source file, and line number.
+
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-    // Request a greeting message.
-    message, err := greetings.Hello("Gladys")
-    // If an error was returned, print it to the console and
-    // exit the program.
+    names := []string{"Gladys", "Samantha", "Darrin", "kazuki"}
+
+ 
+    messages, err := greetings.Hellos(names)
     if err != nil {
         log.Fatal(err)
     }
 
-    // If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(message)
+    fmt.Println(messages)
 }
