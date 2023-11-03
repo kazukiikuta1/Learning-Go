652ad354d5fbbff63155c5d3ff016a5ea058e8cb
生田一樹 <i.k@ikutakazukinoMacBook-Pro.local>
2023-11-03 17:39:05 +0900

    go学習

--- hello/hello.go
+++ hello/hello.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "example.com/greetings"
+    "fmt"
+    "log"
+
+    "example.com/greetings"
 )
 
 func main() {
-	log.SetPrefix("greetings: ")
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+    // Request a greeting message.
+    message, err := greetings.Hello("Gladys")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(message)
+    // If no error was returned, print the returned message
+    // to the console.
+    fmt.Println(message)
 }
-
